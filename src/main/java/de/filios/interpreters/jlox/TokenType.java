2defa9151822af3e677a26cb9e0def55a7b163b7
Georg@Atlas <gthb@filios.de>
2021-12-19 09:14:45 +0100

    Added Increment&Decrement Tokens

--- src/main/java/de/filios/interpreters/jlox/TokenType.java
+++ src/main/java/de/filios/interpreters/jlox/TokenType.java
@@ -3,12 +3,14 @@ package de.filios.interpreters.jlox;
 public enum TokenType {
     // Single-character tokens.
     LEFT_PAREN , RIGHT_PAREN , LEFT_BRACE , RIGHT_BRACE ,
-    COMMA, DOT, MINUS, PLUS, SEMICOLON, SLASH, STAR, QUESTIONMARK, COLON,
+    COMMA, DOT, SEMICOLON, SLASH, STAR, QUESTIONMARK, COLON,
     // One or two character tokens.
     BANG , BANG_EQUAL ,
     EQUAL, EQUAL_EQUAL,
     GREATER, GREATER_EQUAL,
     LESS, LESS_EQUAL,
+    MINUS, MINUS_MINUS,
+    PLUS, PLUS_PLUS,
     // Literals.
     IDENTIFIER , STRING , NUMBER ,
     //Keywords.
